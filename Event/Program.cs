d7b4778ad0ac36817adf3c1b78b86c11e50266cd
Serg <Serg@DESKTOP-MQHCFJJ>
2022-03-23 16:59:57 +0300

    Добавьте файлы проекта.

---
+++ Event/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var klava = new Keyboard();
+            klava.OnKeyPressed += (sender, key) => Console.WriteLine("\n" + key + " - это не 'c'");
+            klava.Press();
+        }
+    }
+    class Keyboard
+    {
+        public event EventHandler<char> OnKeyPressed;
+        public void Press()
+        {
+            char key = Console.ReadKey().KeyChar;
+            while (key != 'c' && key != 'с')
+            {
+                OnKeyPressed?.Invoke(this, key);
+                key = Console.ReadKey().KeyChar;
+            }
+        }
+    }
+}
